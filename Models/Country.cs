5481372bc4ae2cf6b771653a27a5988c77814f15
Leonardo Santos Severino <leonardossev@gmail.com>
2018-12-31 22:00:43 -0200

    Creates the Country entity

---
+++ Models/Country.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebAPI.Models
+{
+    public partial class Country
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int? Population { get; set; }
+    }
+}
